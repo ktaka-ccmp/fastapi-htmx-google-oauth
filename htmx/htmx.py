a9f44d64ce374b3f0498e4138161fa89b6e164fd
Kimitoshi Takahashi <ktaka@ccmp.jp>
2024-01-24 05:13:10 +0900

    Massive improvement.

--- htmx/htmx.py
+++ htmx/htmx.py
@@ -1,7 +1,7 @@
 from typing import Optional, Annotated
 from fastapi import APIRouter, Request, HTTPException, status, Depends, Header, Cookie
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 
 from sqlalchemy.orm import Session
 from data.db import Customer, get_db
@@ -14,7 +14,7 @@ async def spa(request: Request):
     context = {"request": request, "title": "Spa Top Page"}
     return templates.TemplateResponse("spa.j2", context)
 
-@router.get("/spa.content", response_class=HTMLResponse)
+@router.get("/content", response_class=HTMLResponse)
 async def spa_content(request: Request, hx_request: Optional[str] = Header(None)):
     if not hx_request:
         raise HTTPException(
@@ -22,30 +22,28 @@ async def spa_content(request: Request, hx_request: Optional[str] = Header(None)
             detail="Only HX request is allowed to this end point."
             )
     context = {"request": request, "title": "Spa Top Page"}
-    return templates.TemplateResponse("spa.content.j2", context)
+    return templates.TemplateResponse("content.j2", context)
 
 # HTMX Response function
-@router.get("/list.tbody", response_class=HTMLResponse)
-async def list_tbody(request: Request, skip: int = 0, limit: int = 1, reset: bool = False, hx_request: Optional[str] = Header(None), db: Session = Depends(get_db)):
+@router.get("/content.list.tbody", response_class=HTMLResponse)
+async def content_list_tbody(request: Request, skip: int = 0, limit: int = 1, reset: bool = False, hx_request: Optional[str] = Header(None), db: Session = Depends(get_db)):
     if not hx_request:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Only HX request is allowed to this end point."
             )
     context = get_customer_table_context(request, skip, limit, reset, db)
-    if reset:
-        return templates.TemplateResponse("list.tbody_empty.j2", context)
-    return templates.TemplateResponse("list.tbody.j2", context)
+    return templates.TemplateResponse("content.list.tbody.j2", context)
 
-@router.get("/list.content", response_class=HTMLResponse)
-async def list_content(request: Request, skip: int = 0, limit: int = 1, reset: bool = False, hx_request: Optional[str] = Header(None), db: Session = Depends(get_db)):
+@router.get("/content.list", response_class=HTMLResponse)
+async def content_list(request: Request, skip: int = 0, limit: int = 2, reset: bool = False, hx_request: Optional[str] = Header(None), db: Session = Depends(get_db)):
     if not hx_request:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Only HX request is allowed to this end point."
             )
     context = get_customer_table_context(request, skip, limit, reset, db)
-    return templates.TemplateResponse("list.content.j2", context)
+    return templates.TemplateResponse("content.list.j2", context)
 
 def get_customer_table_context(request, skip, limit, reset, db):
     customers = db.query(Customer).offset(skip).limit(limit).all()
@@ -55,3 +53,12 @@ def get_customer_table_context(request, skip, limit, reset, db):
         skip_next = skip+limit
     context = {"request": request, "skip_next": skip_next, "limit": limit, 'customers': customers, "title": "Incremental hx-get demo"}
     return context
+
+
+@router.get("/icon.png")
+async def main():
+    return FileResponse("images/unknown-person-icon.png")
+
+@router.get("/logout.png")
+async def main():
+    return FileResponse("images/door-check-out-icon.png")
--- htmx/htmx_secret.py
+++ htmx/htmx_secret.py
@@ -7,22 +7,22 @@ router = APIRouter()
 templates = Jinja2Templates(directory='templates')
 
 # Normal Response function
-@router.get("/secret1", response_class=HTMLResponse)
-async def secret1(request: Request, hx_request: Optional[str] = Header(None), session_id: Annotated[str | None, Cookie()] = None,):
+@router.get("/content.secret1", response_class=HTMLResponse)
+async def content_secret1(request: Request, hx_request: Optional[str] = Header(None), session_id: Annotated[str | None, Cookie()] = None,):
     if not hx_request:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Only HX request is allowed to this end point."
             )
     context = {"request": request, "title": "You've reached Secret Page #1 !"}
-    return templates.TemplateResponse("secret.j2", context)
+    return templates.TemplateResponse("content.secret.j2", context)
 
-@router.get("/secret2", response_class=HTMLResponse)
-async def secret2(request: Request, hx_request: Optional[str] = Header(None)):
+@router.get("/content.secret2", response_class=HTMLResponse)
+async def content_secret2(request: Request, hx_request: Optional[str] = Header(None)):
     if not hx_request:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Only HX request is allowed to this end point."
             )
     context = {"request": request, "title": "You've reached Secret Page #2 !"}
-    return templates.TemplateResponse("secret.j2", context)
+    return templates.TemplateResponse("content.secret.j2", context)
